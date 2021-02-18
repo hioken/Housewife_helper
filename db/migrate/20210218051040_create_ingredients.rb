7fc446b5fb5c0d1e834462fec202516e538c499e
Ubuntu <ubuntu@ip-172-31-25-240.ap-southeast-1.compute.internal>
2021-02-18 07:16:38 +0000

    first_ingredient

---
+++ db/migrate/20210218051040_create_ingredients.rb
@@ -0,0 +1,12 @@
+class CreateIngredients < ActiveRecord::Migration[6.1]
+  def change
+    create_table :ingredients, id: false do |t|
+      t.column :code, 'int(4) PRIMARY KEY'
+      t.string :name, null: false
+      t.integer :unit null: false
+      t.integer :html_color, null: false
+
+      t.timestamps
+    end
+  end
+end
