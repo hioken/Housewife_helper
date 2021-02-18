05d08e3cf411bebdab3fd4116addfacf4067954c
Ubuntu <ubuntu@ip-172-31-25-240.ap-southeast-1.compute.internal>
2021-02-18 21:50:57 +0000

    first__mypage

--- app/controllers/end_users_controller.rb
+++ app/controllers/end_users_controller.rb
@@ -3,5 +3,11 @@ class EndUsersController < ApplicationController
   end
   
   def update
+    current_user.update(end_user_params)
   end
+  
+  private
+    def end_user_params
+      params.require(:end_user).permit(:famiry_size, :cooking_time_limit)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :end_users, skip: :password
   
-  resource :users, only: [:show, :update]
+  resource :end_users, only: [:show, :update]
+  root 'end_users#show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
