72d0ff92bcb6404e6bb7c3c2790dcead8be8c535
Ubuntu <ubuntu@ip-172-31-25-240.ap-southeast-1.compute.internal>
2021-02-18 18:49:17 +0000

    exce__ingredientの主キーをidに変更、seedの修正

--- db/migrate/20210218051040_create_ingredients.rb
+++ db/migrate/20210218051040_create_ingredients.rb
@@ -1,9 +1,9 @@
 class CreateIngredients < ActiveRecord::Migration[6.1]
   def change
     create_table :ingredients, id: false do |t|
-      t.column :code, 'int(4) PRIMARY KEY'
+      t.column :id, 'int(4) PRIMARY KEY'
       t.string :name, null: false
-      t.integer :unit null: false
+      t.integer :unit, null: false
       t.integer :html_color, null: false
 
       t.timestamps
---
+++ db/migrate/20210218184451_create_fridge_items.rb
@@ -0,0 +1,11 @@
+class CreateFridgeItems < ActiveRecord::Migration[6.1]
+  def change
+    create_table :fridge_items do |t|
+      t.references :end_user, null: false, foreign_key: true
+      t.references :ingredient, null: false, foreign_key: true, index: false
+      t.integer :amount, null: false
+
+      t.timestamps
+    end
+  end
+end
