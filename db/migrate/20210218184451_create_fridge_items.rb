8a814ba68ad18e5703bd2cda50eada5f55f10d81
Ubuntu <ubuntu@ip-172-31-25-240.ap-southeast-1.compute.internal>
2021-02-19 07:45:24 +0000

    change__add_index_ingredient_id

--- db/migrate/20210218184451_create_fridge_items.rb
+++ db/migrate/20210218184451_create_fridge_items.rb
@@ -2,7 +2,7 @@ class CreateFridgeItems < ActiveRecord::Migration[6.1]
   def change
     create_table :fridge_items do |t|
       t.references :end_user, null: false, foreign_key: true
-      t.references :ingredient, null: false, foreign_key: true, index: false
+      t.references :ingredient, null: false, foreign_key: true
       t.integer :amount, null: false
 
       t.timestamps
