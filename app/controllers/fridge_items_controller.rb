1acd8828c06c17f3090b1bd3cb228ac5a57aca61
Ubuntu <ubuntu@ip-172-31-25-240.ap-southeast-1.compute.internal>
2021-02-20 08:24:40 +0000

    first_fridge_update  create fridge_items controller

---
+++ app/controllers/fridge_items_controller.rb
@@ -0,0 +1,10 @@
+class FridgeItemsController < ApplicationController
+  def new
+  end
+  
+  def create
+  end
+  
+  def update
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   resource :end_users, only: [:show, :update]
-  devise_for :end_users, skip: :password
+  resources :fridge_items, only: [:new, :create, :update]
   
+  devise_for :end_users, skip: :password
   root 'end_users#show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
