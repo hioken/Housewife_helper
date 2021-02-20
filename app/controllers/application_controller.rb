24b49254a64eedfa96c286afccefe1431d304073
Ubuntu <ubuntu@ip-172-31-25-240.ap-southeast-1.compute.internal>
2021-02-20 06:37:19 +0000

    last__mypage

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,38 @@
 class ApplicationController < ActionController::Base
+  
 end
 
 class Integer
-  def regam
-    self.divmod(4)
+  # 割り切れるか
+  def divisible?(number)
+    self % number == 0
+  end
+  
+  # form_select用の配列を返す
+  def amount_select
+    ret = []
+    self.times do |amo|
+      amo += 1
+      ret <<
+        if amo < 4
+          ["#{amo} / 4", self - amo]
+        elsif amo % 4 != 0
+          ["#{amo / 4}と#{amo % 4} / 4 ", self - amo]
+        else
+          [(amo / 4).to_s, self - amo]
+        end
+    end
+    ret
+  end
+  
+  # グラム表記の食材のform_select用の配列を返す
+  def gram_select
+    ret = []
+    (self / 200).times do |amo|
+      amo += 1
+      amo *= 200
+      ret << [(amo / 4).to_s, self - amo]
+    end
+    ret
   end
 end
--- app/controllers/end_users_controller.rb
+++ app/controllers/end_users_controller.rb
@@ -1,22 +1,22 @@
 class EndUsersController < ApplicationController
   before_action :authenticate_end_user!
   def show
-    1000.times do 
-      foods = current_end_user.fridge_items.joins(:ingredient).pluck(:ingredient_id, :name, :amount, :unit, :html_color)
-      @meats_fishes = []
-      @vegetables = []
-      @others = []
-      @seasonings = [] 
-      foods.each do |food|
-        if food[0] > 4999
-          @seasonings << food
-        elsif food[0] > 2999
-          @others << food
-        elsif food[0] > 999
-          @vegetables << food
-        else
-          @meats_fishes << food
-        end
+    # ユーザーの冷蔵庫の情報を配列で取得、SQLを減らすために一括受取
+    foods = current_end_user.fridge_items.joins(:ingredient).pluck(:ingredient_id, :name, :amount, :unit, :html_color)
+    # ジャンル別に分割
+    @meats_fishes = []
+    @vegetables = []
+    @others = []
+    @seasonings = [] 
+    foods.each do |food|
+      if food[0] > 4999
+        @seasonings << food
+      elsif food[0] > 2999
+        @others << food
+      elsif food[0] > 999
+        @vegetables << food
+      elsif food[0] > 99
+        @meats_fishes << food
       end
     end
   end
