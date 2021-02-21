848740387815dab71279305ead576ba2891d8021
Ubuntu <ubuntu@ip-172-31-29-229.ap-southeast-1.compute.internal>
2021-02-21 01:27:31 +0000

    check_冷蔵庫update完成

--- app/controllers/fridge_items_controller.rb
+++ app/controllers/fridge_items_controller.rb
@@ -15,19 +15,19 @@ class FridgeItemsController < ApplicationController
       if code > 4999 or code < 100
         @foods = current_end_user.fridge_items.joins(:ingredient).where(ingredient_id: 5000..9999).pluck(:ingredient_id, :name, :amount, :unit, :html_color, 'fridge_items.id')
         @food_box_id = 'seasonings'
-        @foods_genre = '調味料/穀物'
+        @food_genre = '調味料/穀物'
       elsif code > 2999
         @foods = current_end_user.fridge_items.joins(:ingredient).where(ingredient_id: 2999..4999).pluck(:ingredient_id, :name, :amount, :unit, :html_color, 'fridge_items.id')
         @food_box_id = 'others'
-        @foods_genre = 'その他' 
+        @food_genre = 'その他' 
       elsif code > 999
         @foods = current_end_user.fridge_items.joins(:ingredient).where(ingredient_id: 1000..1999).pluck(:ingredient_id, :name, :amount, :unit, :html_color, 'fridge_items.id')
         @food_box_id = 'vegetables'
-        @foods_genre = '野菜'
+        @food_genre = '野菜'
       elsif code > 99
         @foods = current_end_user.fridge_items.joins(:ingredient).where(ingredient_id: 100..999).pluck(:ingredient_id, :name, :amount, :unit, :html_color, 'fridge_items.id')
         @food_box_id = 'meats_fishes'
-        @foods_genre = '肉/魚'
+        @food_genre = '肉/魚'
       else
         raise "想定されていない食材コードです|| code: #{code}, controller: fridge_items_controller"
       end
