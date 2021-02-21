1825e7b1d10711f29b679748e4100d208b765a88
Ubuntu <ubuntu@ip-172-31-29-229.ap-southeast-1.compute.internal>
2021-02-21 03:08:06 +0000

    change_食材コードの変更と、定数の定義

--- app/models/fridge_item.rb
+++ app/models/fridge_item.rb
@@ -1,5 +1,14 @@
 class FridgeItem < ApplicationRecord
-  # assciation
+  # Database
+  
+  ## Table
+  ### assciation
   belongs_to :end_user
   belongs_to :ingredient
+  
 end
+
+=begin
+  scope :all_genre, -> 
+  scope :scope_genre, -> (genre) {joins(:ingredient).where(ingredient_id: genre, user_id: current_end_user)}
+=end
