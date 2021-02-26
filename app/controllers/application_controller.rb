76f4e7fe8240b5bcababfe8f72f4012172c31dac
hioken <radcloseff@gmail.com>
2021-02-26 04:22:59 +0000

    マイページのselectメソッド用メソッドを、グラム表記でも同じメソッドを利用できるように改良

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,29 +10,27 @@ class Integer
   end
   
   # form_select用の配列を返す
-  def amount_select
+  def amount_select(unit)
     ret = []
-    self.times do |amo|
-      amo += 1
-      ret <<
-        if amo < 4
-          ["#{amo} / 4", self - amo]
-        elsif amo % 4 != 0
-          ["#{amo / 4}と#{amo % 4} / 4 ", self - amo]
-        else
-          [(amo / 4).to_s, self - amo]
-        end
-    end
-    ret
-  end
-  
-  # グラム表記の食材のform_select用の配列を返す
-  def gram_select
-    ret = []
-    (self / 200).times do |amo|
-      amo += 1
-      amo *= 200
-      ret << [(amo / 4).to_s, self - amo]
+    if unit != 'g'
+      self.times do |amo|
+        amo += 1
+        ret <<
+          if amo < 4
+            ["#{amo} / 4" + unit, self - amo]
+          elsif amo % 4 != 0
+            ["#{amo / 4}と#{amo % 4} / 4 #{unit}", self - amo]
+          else
+            [(amo / 4).to_s + unit, self - amo]
+          end
+      end
+    else
+      (self / 200).times do |amo|
+        amo += 1
+        amo *= 200
+        ret << [(amo / 4).to_s + unit, self - amo]
+      end
+      ret << ["#{(self / 4)}g", 0] if self / 200 == 0
     end
     ret
   end
