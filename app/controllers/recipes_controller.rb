2ddb4b6c9afb9addb949067a9b28881218d754ac
hioken <radcloseff@gmail.com>
2021-02-26 04:30:31 +0000

    first_create controller recipes

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,22 @@
+class RecipesController < ApplicationController
+  def top
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+  
+  def create
+  end
+  
+  def update
+  end
+end
