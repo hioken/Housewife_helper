491ad24f596a4a84f46294df11147fd01c52bc3a
hioken <radcloseff@gmail.com>
2021-03-03 23:11:03 +0900

    change_recipe_index高速化 cloud9のせいでデータ壊れているかも

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,7 +3,13 @@ class RecipesController < ApplicationController
   end
 
   def index
-    @recipes = Recipe.eager_load(:recipe_ingredients).limit(50)
+    @recipes = Recipe.all
+    @ingredients = {}
+    @recipes.each { |recipe| @ingredients[recipe.id] = {}}
+    @recipes.joins(:recipe_ingredients).where('recipe_ingredients.ingredient_id': ApplicationRecord::GENRE_SCOPE[:not_seasoning]).pluck(:id, :ingredient_id, :amount).each do |data|
+      @ingredients[data[0]][data[1]] = data[2]
+    end
+    @fridge_items = current_end_user.fridge_items.where(ingredient_id: ApplicationRecord::GENRE_SCOPE[:not_seasoning]).pluck(:ingredient_id, :amount).to_h
   end
 
   def show
