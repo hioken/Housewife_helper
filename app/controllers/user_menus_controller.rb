866d830d50b0cb3a943c54bc2b69c1b6d6fdf75d
hioken <radcloseff@gmail.com>
2021-03-05 03:30:28 +0900

    check_

--- app/controllers/user_menus_controller.rb
+++ app/controllers/user_menus_controller.rb
@@ -5,6 +5,12 @@ class UserMenusController < ApplicationController
 	end
 	
 	def new
+    @ingredients = {}
+    @recipes.each { |recipe| @ingredients[recipe.id] = {}}
+    @recipes.joins(:recipe_ingredients).where('recipe_ingredients.ingredient_id': ApplicationRecord::GENRE_SCOPE[:not_seasoning]).pluck(:id, :ingredient_id, :amount).each do |data|
+      @ingredients[data[0]][data[1]] = data[2]
+    end
+    @fridge_items = current_end_user.fridge_items.where(ingredient_id: ApplicationRecord::GENRE_SCOPE[:not_seasoning]).pluck(:ingredient_id, :amount).to_h
 	end
 	
 	def new_week
