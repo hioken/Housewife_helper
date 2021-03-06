eead0c819d2dbab25d66fb78a253ae947d82e72b
hioken <radcloseff@gmail.com>
2021-03-07 03:36:13 +0900

    check_

--- app/controllers/user_menus_controller.rb
+++ app/controllers/user_menus_controller.rb
@@ -11,8 +11,34 @@ class UserMenusController < ApplicationController
 	end
 	
 	def new_week
-		@recipes = recommend(params[:days] ? params[:days].to_i - 1 : 6)
-		
+		if params[:change]
+		else
+			days = params[:days] ? params[:days].to_i : 7
+			@recipes = recommend(days, current_end_user.family_size, recipe_only: true)
+			week_menu = current_end_user.user_menus.where(cooking_date: (Date.today)..(Date.today + days - 1)).pluck(:cooking_date, :recipe_id, :sarve)
+			@sarves = {}
+			week_menu.each do |date, id, sarve|
+				@sarves[id] = sarve
+				@recipe.insert((date - Date.today).to_i, [id, sarve])
+			end
+			@recipe.slice!(-(week_menu.size)...-1)
+			@lacks = {}
+			@recipes.preload(:recipe_ingredients).each do |recipe|
+				sarve = @sarves[recipe.id] ? @sarves[recipe.id] : current_end_user.family_size
+				recipe.recipe_ingredients.each do |ingredient|
+					@lacks[ingredient.ingredient_id] = @lacks[ingredient.ingredient_id].to_i + ingredient.amount * sarve
+				end
+			end
+			current_end_user.fridge_items.pluck(:ingredient_id, :amount).each do |id, amount|
+				@lacks[id] -= amount if @lacks[id]
+			end
+			@lacks.delete_if{ |id, amount| amount <= 0 }
+		end
+	end
+	
+	def new_week_change
+		recipe = Recipe.find(params[:recipe])
+		recipe.recipe_ingredients.each do ||
 	end
 	
 	def create
@@ -73,21 +99,31 @@ class UserMenusController < ApplicationController
       params.require(:user_menu).permit(:cooking_date, :sarve, :recipe_id)
     end
     
-    def recommend(quantity = 4, sarve = 1, limit: 50, only_recipe: false)
+    def recommend(quantity = 4, sarve = 1, limit: 50, class_relation: false, duplicate: true, recipe_only: true)
 			# レシピの冷蔵庫の中身で賄える量を計算、40%以上を取得
 			recipes = Recipe.eager_load(:recipe_ingredients).limit(limit)
 			fridge = current_end_user.fridge_items.where(ingredient_id: FridgeItem::GENRE_SCOPE[:not_seasoning]).pluck(:ingredient_id, :amount).to_h
 			cover_how = {}
+			week_menu = current_end_user.user_menu.where(cooking_date: (Date.today)..(Date.today + 6)).pluck(:recipe_id) # 今週のレシピを取得
+			
 			recipes.each do |recipe|
+				next if week_menu.include?(recipe.id) # 今週のレシピに含まれているレシピはスルー
 				cover_cnt, ingredient_cnt = 0, 0
-				recipe.recipe_ingredients.each do |ingredient|
-					id = ingredient.ingredient_id
-					next unless RecipeIngredient::GENRE_SCOPE[:not_seasoning].include?(id)
+				ingredients = recipe.recipe_ingredients.pluck(:ingredient_id, :amount).to_h
+				ingredients.each do |id, amount|
+					next unless RecipeIngredient::GENRE_SCOPE[:not_seasoning].include?(id) # 調味料は考慮しない
 					ingredient_cnt += 1
-					cover_cnt += 1 if fridge[id] && fridge[id] - ingredient.amount * sarve >= 0
+					cover_cnt += 1 if fridge[id] && fridge[id] - amount * sarve >= 0
 				end
     		how = (cover_cnt * 100 / ingredient_cnt)
-				cover_how[recipe.id] = how if how >= 40
+    		
+    		# メニュー候補追加処理
+    		if duplicate && how >= 40 
+					cover_how[recipe.id] = how
+				elsif how >= 60 # duplicateがfalseの時は、60%以上を賄えるメニューが見つかるたび、冷蔵庫の中身を減らす
+					cover_how[recipe.id] = how 
+					ingredients.each { |id, amount| fridge[id] -= amount if fridge[id] } 
+				end
 			end
 			
 			# 上の処理で残ったレシピが4つになるように調整
@@ -99,7 +135,13 @@ class UserMenusController < ApplicationController
 					cover_how[id] = 0 unless cover_how.key?(id)
 				end
 			end
-			recipes = Recipe.where(id: cover_how.keys).to_a.sort_by{|data| cover_how[data.id] }.reverse
-			recipes.map { |recipe| [recipe, cover_how[recipe.id]] } unless only_recipe
+			
+			# class_relationがtrueの時は、レシピデータをRelationで返す、そうでない場合、Arrayで、さらにrecipe_onlyがfalseの場合は賄えている割合付きの２次元配列返す
+			recipes = Recipe.where(id: cover_how.keys)
+			if recipe_only
+				recipes.pluck(:id)
+			elsif !(class_relation)
+				recipes.sort_by{|data| cover_how[data.id] }.reverse.map { |recipe| [recipe, cover_how[recipe.id]] } 
+			end
     end
 end
