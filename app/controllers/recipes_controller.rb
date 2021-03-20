4accb893061197f904146967d4b90e3989a96113
hioken <radcloseff@gmail.com>
2021-03-21 04:59:11 +0900

    first_refactoring

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -28,12 +28,14 @@ class RecipesController < ApplicationController
   def new
   end
 
-  def edit
-  end
-  
   def create
+  end
+
+=begin
+  def edit
   end
   
   def update
   end
+=end  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,11 +6,12 @@ Rails.application.routes.draw do
   resource :need_ingredients, only: [:show]
   resources :fridge_items, only: [:new, :create, :update]
   
-  resources :recipes, except: [:destroy]
-  patch 'user_menu/:id/cooked', to: 'user_menus#cooked', as: :cooked_user_menu
-  get 'user_menus/new_week', to: 'user_menus#new_week', as: :new_week_user_menu
+  # resources :recipes, except: [:destroy]
+  resources :recipes, except: [:destroy, :edit, :update, :new, :create]
+  get 'user_menus/new_week' => 'user_menus#new_week', as: :new_week_user_menu
   resources :user_menus, except: [:show, :edit]
   
+  # resource :ingredients, only: [:new, :create]
   devise_for :end_users, skip: :password
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
